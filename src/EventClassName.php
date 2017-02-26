30c39e5d7655a2132da45b82d4691e851d786db5
Paul Court <g@rgoyle.com>
2017-02-26 14:59:02 +0000

    Renamed class to match more common usage.

--- src/EventClassName.php
+++ src/EventClassName.php
@@ -3,11 +3,10 @@
 namespace Pmc\EventSourceLib;
 
 /**
- * @author Paul Court <emails@paulcourt.co.uk>
+ * @author Gargoyle <g@rgoyle.com>
  */
-class EventName extends \Pmc\ObjectLib\BasicString
+class EventClassName extends \Pmc\ObjectLib\BasicString
 {
-
     public function __construct(string $value)
     {
         parent::__construct($value, false, 255);
